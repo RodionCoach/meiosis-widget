1d9dcba2fd2bd3dac6677e92d316516670d0fc16
Rodion <www.rodionkochetkow@gmail.com>
2021-02-01 21:54:02 +0300

    widget p.2

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import { Timeline } from "./plugins/timeline";
+import "./index.css";
+
+const popup = document.getElementById("timeline-wrapper");
+const titles = document.getElementsByClassName("title");
+let timeline;
+
+const seqCount = {
+  "INT3D-77-EarlyMeiosis1": 400,
+  "INT3D-78-LateMeiosis1": 440,
+  "INT3D-79-EarlyMeiosis2": 251,
+  "INT3D-80-LateMeiosis2": 1008,
+  "INT3D-119-Allphasesofmeiosis": 1594,
+}
+
+const showVideo = (id) => {
+  popup.classList.add("opened");
+
+  timeline = new Timeline({
+    containerId: "timeline",
+    namePattern: id,
+    fileExtension: "jpg",
+    framesCount: seqCount[id],
+    framesFolder: `/static/${id}`,
+    fps: 15
+  });
+
+  timeline.init();
+};
+
+const closePopUp = () => {
+  if (popup.classList.contains("opened")) {
+    popup.classList.remove("opened");
+    timeline.destroy();
+  }
+}
+
+for (let title of titles) {
+  title.addEventListener("click", () => {
+    showVideo(title.getAttribute("data-point"))
+  });
+}
+
+document.getElementById("closeButton").addEventListener("click", () => {
+  closePopUp();
+});
